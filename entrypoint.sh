638ccaf7601d12b47536a87a25fd0403cdcd2014
romcheck <romcheck@gmail.com>
2017-01-09 21:32:32 +0200

    add crond run in foreground mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,5 @@ cat <<EOF >/etc/periodic/daily/delete-indices
 #!/bin/sh
 /usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":14},{"filtertype":"pattern","kind":"prefix","value":"logstash-"}]' --ignore_empty_list
 EOF
+
+crond -f
