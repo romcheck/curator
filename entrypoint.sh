ce377e6ee612ac9ed4a9b39916fd059836ce03ea
romcheck <romcheck@gmail.com>
2017-01-10 10:59:57 +0200

    add ES_DAYS env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cat <<EOF >/etc/periodic/daily/delete-indices
+cat <<EOF >./123
 #!/bin/sh
-/usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":14},{"filtertype":"pattern","kind":"prefix","value":"logstash-"}]' --ignore_empty_list
+/usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":${ES_DAYS:-14}},{"filtertype":"pattern","kind":"prefix","value":"logstash-"}]' --ignore_empty_list
 EOF
 
 exec "$@"
