34625aefadba37496d9a1c6775349c4c6c98d700
romcheck <romcheck@gmail.com>
2018-10-03 20:13:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF > /etc/periodic/daily/deleteindices
 #!/bin/sh
-/usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":${ES_DAYS:-14}},{"filtertype":"pattern","kind":"prefix","value":"logstash-"}]' --ignore_empty_list
+/usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --ignore_empty_list --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":${ES_DAYS:-14}}]'
 EOF
 
 chmod +x /etc/periodic/daily/deleteindices
