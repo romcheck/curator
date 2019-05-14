1879e50c6e3750490d5ce26f5dfa9fd84c2a689b
romcheck <romcheck@gmail.com>
2019-05-14 20:30:50 +0200

    upd excluded kibana index prefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF > /etc/periodic/daily/deleteindices
 #!/bin/sh
-/usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --ignore_empty_list --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":${ES_DAYS:-14}},{"filtertype":"kibana","exclude":true}]'
+/usr/local/bin/curator_cli --host ${ES_HOST:-elasticsearch} --port ${ES_PORT:-9200} delete_indices --ignore_empty_list --filter_list '[{"filtertype":"age","source":"creation_date", "direction":"older","unit":"days","unit_count":${ES_DAYS:-14}},{"filtertype":"pattern","kind":"prefix","value":".kibana","exclude":true}]'
 EOF
 
 chmod +x /etc/periodic/daily/deleteindices
